bd470f3375de9660d890358e87fc47c54727210c
tirthb101 <bhatiatirth01@gmail.com>
2022-10-05 15:38:50 +0530

    added

---
+++ TowarOfHanoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void toh(int no_disk, char from, char to, char aux);
+
+void main()
+{
+    toh(2, 'A', 'C', 'B');
+}
+
+void toh(int no_disk, char from, char to, char aux)
+{
+    if (no_disk > 1)
+    {
+        toh(no_disk - 1, from, aux, to);
+        printf("Move  disk from %c to %c.\n", from, to);
+
+        toh(no_disk - 1, aux, to, from);
+    }
+    else if (no_disk == 1)
+    {
+        printf("Move disk from %c to %c.\n", from, to);
+    }
+}
